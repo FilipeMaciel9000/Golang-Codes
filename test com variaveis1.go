732fa602930c44107570f2735874f352dbe66583
FilipeMaciel9000 <83095857+FilipeMaciel9000@users.noreply.github.com>
2021-07-01 10:38:58 -0300

    Update test com variaveis1.go

--- test com variaveis1.go
+++ test com variaveis1.go
@@ -1,10 +1,10 @@
 //Usando variáveis pela primeira vez
 /*
 Entendendo a estrutura de um programa em go:
-1) Declaração de pacote principal do nesso programa, é por esse pacote que o programa ira começar;
+1) Declaração de pacote principal do nosso programa, é por esse pacote que o programa irá começar;
 2) Declaração do comando import, para importar códigos úteis de outros pacotes;
 3) "fmt" é abreviação de format, que implementa formatação de input e output para o nosso código;
-4) Declaração da função principal do nesso programa, é a porta de entrada e de saida do seu programa em go
+4) Declaração da função principal do nosso programa, é a porta de entrada e de saida do seu programa em go
 */
 package main //(1)
 
@@ -15,7 +15,7 @@ import ( //(2)
 func main() { //(4)
 	//Aqui seu código começa;
 	//Usamos o operador curto de declaração(A marmota :=) para declarar três variáveis diferentes em apenas uma linha;
-	//Preste atenção na formatação: três strings diferentes foram criadas e atribuidas para três variaveis diferentes separadas por vírgula;
+	//Preste atenção na formatação: três strings diferentes foram criadas e atribuidas para três variáveis diferentes separadas por vírgula;
 	//Mas detalhes sobre tipos de variáveis serão vistos em test com variaveis2;
 	x, y, z := "Olá,", "Mundo!", "Pronto me livrei da maldição"
 	//O comando usado para mostrar a variável na tela é fmt(package).Printf(identificador);
@@ -23,6 +23,7 @@ func main() { //(4)
 	//Preste atenção na formatação: Com fmt.Printf("%v", x) mostramos o valor que está contido na variável x;
 	//O \n em "%v %v\n" serve para pular uma linha;
 	//Assim fmt.Printf("%v %v\n", x, y) mostra os valores atribuidos a x e y e pulará uma linha;
+	//Enquanto fmt.Printf(z) mostrará apenas o valor contido da variável z; 
 	fmt.Printf("%v %v\n", x, y)
 	fmt.Printf(z)
 }
